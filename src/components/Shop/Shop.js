9320dc7f53f63af0bd7716f268a6299e33d4936d
MdAnowar1 <anowarplus1014@gmail.com>
2022-03-24 21:47:36 +0600

    first commi ema jhon logo

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+         <div className="products-container">
+         <h3>This is Products</h3>
+         </div>
+         <div className="cart-container">
+         <h3>Order Summary</h3>
+         </div>
+        </div>
+    );
+};
+
+export default Shop;
